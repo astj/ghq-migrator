919d140f4a5998400c4bd2c0cf045f7ca9b9a19f
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-12-12 13:17:47 +0900

    まとめる

--- ghq-migrate.bash
+++ ghq-migrate.bash
@@ -3,7 +3,7 @@ TARGET_DIR=$1
 GHQ_ROOT=$(ghq root)
 
 if ! [ -d $TARGET_DIR ]; then
-    echo "directory needed!"
+    echo "directory needed! : ${TARGET_DIR}"
     exit 1
 fi
 
@@ -29,8 +29,10 @@ function _remote_path_from_url {
     echo $REMOTE_PATH
 }
 
-if [ $N_OF_REMOTES -eq 1 ]; then
-    REMOTE_PATH=$(_remote_path_from_url $(cd $TARGET_DIR;git config --get-regexp remote.*.url | cut -d ' ' -f 2))
+function _move_repository_directory {
+    TARGET_DIR=$1
+    REMOTE_PATH=$2
+
     echo "move this repository to ${GHQ_ROOT}/${REMOTE_PATH}"
     if [ ${GHQ_MIGRATOR_ACTUALLY_RUN:-0} -eq 1 ]; then
         NEW_REPO_DIR="${GHQ_ROOT}/${REMOTE_PATH}"
@@ -43,6 +45,11 @@ if [ $N_OF_REMOTES -eq 1 ]; then
     else
         echo 'specify GHQ_MIGRATOR_ACTUALLY_RUN=1 to work actually'
     fi
+}
+
+if [ $N_OF_REMOTES -eq 1 ]; then
+    REMOTE_PATH=$(_remote_path_from_url $(cd $TARGET_DIR;git config --get-regexp remote.*.url | cut -d ' ' -f 2))
+    _move_repository_directory $TARGET_DIR $REMOTE_PATH
 else
     echo "multiple remote detected!!!"
     echo '';echo ''
@@ -53,18 +60,7 @@ else
     if [ $N_OF_ORIGIN_REMOTES -eq 1 ] && [ ${GHQ_MIGRATOR_PREFER_ORIGIN:-0} -eq 1 ]; then
         REMOTE_PATH=$(_remote_path_from_url $(cd $TARGET_DIR;git config --get-regexp remote.origin.url | cut -d ' ' -f 2))
         echo "Use origin"
-        echo "move this repository to ${GHQ_ROOT}/${REMOTE_PATH}"
-        if [ ${GHQ_MIGRATOR_ACTUALLY_RUN:-0} -eq 1 ]; then
-            NEW_REPO_DIR="${GHQ_ROOT}/${REMOTE_PATH}"
-            if [ -e $NEW_REPO_DIR ]; then
-                echo "${NEW_REPO_DIR} already exists!!!!"
-                exit 1
-            fi
-            mkdir -p "${NEW_REPO_DIR%/*}"
-            mv ${TARGET_DIR%/} $NEW_REPO_DIR
-        else
-            echo 'specify GHQ_MIGRATOR_ACTUALLY_RUN=1 to work actually'
-        fi
+        _move_repository_directory $TARGET_DIR $REMOTE_PATH
     else
         echo "We cannot decide which remote to use..."
         exit 1
